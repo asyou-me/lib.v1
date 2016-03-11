140a73d23dc668843cc78b84869ffd3dd3276143
xiaobai <xiaobai@pingxx.com>
2016-03-11 16:57:02 +0800

    update

--- log_client/kafka_handle.go
+++ log_client/kafka_handle.go
@@ -13,9 +13,14 @@ func NewKafkaHandle(lconf LogConf, log *Logger) (*KafkaHandle, error) {
 	config := sarama.NewConfig()
 	config.Producer.RequiredAcks = sarama.WaitForAll
 	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.Compression = sarama.CompressionSnappy
+	config.Producer.Flush.Frequency = 500 * time.Millisecond
 
+	// 删除可能出现的空格
 	addr_pre := strings.Replace(lconf.Addr, " ", "", -1)
+	// 删除可能出现两边的，
 	strings.Trim(",", addr_pre)
+	// 通过[,]分割多个地址
 	addrs := strings.Split(addr_pre, ",")
 	producer, err := sarama.NewSyncProducer(addrs, config)
 	if err != nil {
@@ -53,13 +58,14 @@ func (r *KafkaHandle) WriteTo(msg LogBase) {
 	if r.log.PrintKey {
 		fmt.Println(msg)
 	}
+
+	// 格式化数据到json
 	reader := jsonFormat(msg)
+	// 生成消息主体
 	message := &sarama.ProducerMessage{Topic: r.Topic, Key: sarama.StringEncoder("log"),
 		Value: sarama.ByteEncoder(reader)}
-
-	if _, _, err := r.Producer.SendMessage(message); err != nil {
-		return
-	}
+	// 发送消息到kafka
+	_, _, err := r.Producer.SendMessage(message)
 
 	r.mu.Lock()
 	r.num = r.num + 1
@@ -80,6 +86,22 @@ func (r *KafkaHandle) WriteTo(msg LogBase) {
 
 // kafka处理句柄
 func (r *KafkaHandle) RecoveryTo(msg string) {
+	message := &sarama.ProducerMessage{Topic: r.Topic, Key: sarama.StringEncoder("log"),
+		Value: sarama.StringEncoder(msg)}
+	_, _, err := r.Producer.SendMessage(message)
 
+	r.mu.Lock()
+	r.num = r.num + 1
+	r.mu.Unlock()
+	if err != nil {
+		r.mu.Lock()
+		r.errNum = r.errNum + 1
+		r.mu.Unlock()
+		go func() {
+			r.log.NewsChannel <- msg
+			r.log.Err <- err
+		}()
+		return
+	}
 	msg = ""
 }
--- log_client/log_test.go
+++ log_client/log_test.go
@@ -11,11 +11,9 @@ var logger *Logger
 func Test(t *testing.T) {
 
 	logger, err = New(&[]LogConf{
-		LogConf{"115.29.113.249:9092", "cf_log_queue", "", "", "kafka", false, 10},
-		// 主log服务 192.168.1.131
-		//LogConf{"127.0.0.1:6379", "cf_log_queue", "", "", "redis", false, 10},
-		// 备用log服务
-		//LogConf{"/home/xiaobai/dumps", "cf_log", "", "", "file", true, 1},
+		LogConf{"115.29.113.249:9092", "cf_log_queue", "", "", "kafka", false, 10}, // 主log服务 kafka
+		LogConf{"127.0.0.1:6379", "cf_log_queue", "", "", "redis", false, 10},      // 主log服务 mysql
+		LogConf{"/home/xiaobai/dumps", "cf_log", "", "", "file", true, 1},          // 备用log服务 file
 	})
 	if err != nil {
 		panic(err)
