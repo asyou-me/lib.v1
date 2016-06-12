4a02f6a187425a324feaf7dd363fcb44cd6bb98b
xiaobai <422145328@qq.com>
2016-06-12 12:04:13 +0800

    完成基础功能

---
+++ utils/string.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"math/rand"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+
+func RandBytes(n int) []byte {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return b
+}
