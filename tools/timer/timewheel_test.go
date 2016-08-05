b503feb93d9bd67673e0659735dfd40980d97b67
xiaobai <422145328@qq.com>
2016-08-05 14:13:32 +0800

    更新时间轮测试代码

--- tools/timer/timewheel_test.go
+++ tools/timer/timewheel_test.go
@@ -14,7 +14,7 @@ func callback(datas []interface{}) {
 }
 
 func TestTimeWheel(t *testing.T) {
-	timeWheel := NewTimeWhell(1, 3, callback)
+	timeWheel := NewTimeWhell(1, 10, callback)
 	timeWheel.Start()
 	timeWheel.Add(11, "11秒测试1")
 	timeWheel.Add(11, "11秒测试2")
