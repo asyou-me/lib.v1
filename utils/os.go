34e307aad18d8c5c2a975c1426cb52d00aa6047c
xiaobai <422145328@qq.com>
2016-11-02 08:12:59 +0800

    update

---
+++ utils/os.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// CompletePath 将相对路径转换成绝对路径
+func CompletePath(confPath *string) {
+	if len((*confPath)) > 0 && string((*confPath)[0]) != "/" {
+		var currPath string
+		currPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			panic(err)
+		}
+		*confPath = currPath + "/" + *confPath
+	}
+}
+
+// ListDir 获取指定目录下的所有文件，不进入下一级目录搜索，可以匹配后缀过滤。
+func ListDir(dirPth string, suffix string) (files []string, err error) {
+	files = make([]string, 0, 10)
+	dir, err := ioutil.ReadDir(dirPth)
+	if err != nil {
+		return nil, err
+	}
+
+	PthSep := string(os.PathSeparator)
+	suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+
+	for _, fi := range dir {
+		if fi.IsDir() {
+			continue
+		}
+		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) { //匹配文件
+			files = append(files, dirPth+PthSep+fi.Name())
+		}
+	}
+
+	return files, nil
+}
+
+// WalkDir 获取指定目录及所有子目录下的所有文件，可以匹配后缀过滤。
+func WalkDir(dirPth, suffix string) (files []string, err error) {
+	files = make([]string, 0, 30)
+	suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+
+	err = filepath.Walk(dirPth, func(filename string, fi os.FileInfo, err error) error { //遍历目录
+		if fi.IsDir() { // 忽略目录
+			return nil
+		}
+		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) {
+			files = append(files, filename)
+		}
+		return nil
+	})
+
+	return files, err
+}
